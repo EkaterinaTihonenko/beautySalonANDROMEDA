0bc2c54b585e8ea5b0a75ad9e8b99586d9e6da5e
Ekaterina <ekaterinatihonenko8@gmail.com>
2024-05-14 02:08:16 +0300

    main

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import "./App.css";
 export const App = () => {
   return (
     <div className="app">
-      <Router>
+      <Router basename="/beautySalonANDROMEDA/">
         <HeaderTemplate contentHeader={<Header />} />
         <RoutesItems />
         <Footer />
@@ -16,4 +16,3 @@ export const App = () => {
     </div>
   );
 };
-/*basename="/beautySalonANDROMEDA/" */
